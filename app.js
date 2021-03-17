95b20ec9533af98787b9dffd4a8a51da8a8bf6ef
Parri <emailparri@gmail.com>
2021-03-17 16:58:23 +0000

    Refactored javaScript and added notes

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-let player1score = 0;
-let player2score = 0;
+// Setting defaults
 let targetScore = 'Skate';
 let gameOver = false;
 let targetWord = targetScore;
 targetScore = targetWord.length;
 
+// create players as objects
+const player1 = {
+    score: 0,
+    button: document.querySelector('.player1'),
+    display: document.querySelector('.player-one-score')
+}
+
+const player2 = {
+    score: 0,
+    button: document.querySelector('.player2'),
+    display: document.querySelector('.player-two-score')
+}
 
 // define elements
-const player1Button = document.querySelector('.player1');
-const p1score = document.querySelector('.player-one-score');
-const player2Button = document.querySelector('.player2');
-const p2score = document.querySelector('.player-two-score');
 const resetButton = document.querySelector('.reset');
 const targetScoreInput = document.querySelector('#score-limit')
-const mainBox = document.querySelector('.main-box');
 const p1container = document.querySelector('.player1-container')
 const p2container = document.querySelector('.player2-container')
 
@@ -24,53 +30,44 @@ targetScoreInput.addEventListener('change', function (e) {
     reset();
 })
 
-
-player1Button.addEventListener('click', function (e) {
-    if (!gameOver) {
-        player1score++;
-        p1score.append(innerText = targetWord[player1score - 1]);
-        if (player1score === targetScore) {
-            gameOver = true;
-            p1container.classList.add('loser');
-            p2container.classList.add('winner')
-            p2score.classList.remove('line-through');
-            p2score.innerText = 'Winner!';
-            player1Button.disabled = true;
-            player2Button.disabled = true;
-        }
+// update the DOM with scores
+function updateScore(player, opponent) {
+    if (!gameOver) { // if the game isn't over then...
+        player.score++; // add 1 to the score
+        player.display.append(innerText = targetWord[player.score - 1]); // update the display
     }
+    if (player.score === targetScore) { // check that player score = target score
+        gameOver = true; // set gameOver to true to break the loop
+        player.display.classList.add('loser'); // add loser class the player 
+        opponent.display.classList.add('winner') // add winner class too the opponent
+        opponent.display.classList.remove('line-through'); // remove line-through class from opponent
+        opponent.display.innerText = 'Winner!'; // change opponents display to winner
+        player.button.disabled = true; // disable button
+        opponent.button.disabled = true; // disable button
+    }
+}
+
+// running the score funtion when the buttons are clicked. Passing each player's object as parameters
+player1.button.addEventListener('click', function (e) {
+    updateScore(player1, player2)
 })
 
-player2Button.addEventListener('click', function (e) {
-    if (!gameOver) {
-        player2score++;
-        p2score.append(innerText = targetWord[player2score - 1]);
-        if (player2score === targetScore) {
-            gameOver = true;
-            p2container.classList.add('loser');
-            p1container.classList.add('winner')
-            p1score.classList.remove('line-through');
-            p1score.innerText = 'Winner!';
-            payer2Button.disabled = true;
-            player1Button.disabled = true;
-        }
-    }
+player2.button.addEventListener('click', function (e) {
+    updateScore(player2, player1)
 })
 
 resetButton.addEventListener('click', reset)
 
-// reset the game
+// reset the game. Loop over player one and player 2 to reset the DOM back to new game
 function reset() {
     gameOver = false;
-    player1score = 0;
-    player2score = 0;
-    p1container.classList.remove('loser', 'winner');
-    p2container.classList.remove('loser', 'winner');
-    p1score.classList.add('line-through')
-    p2score.classList.add('line-through')
-    player1Button.removeAttribute('disabled')
-    player2Button.removeAttribute('disabled')
-    p1score.innerText = '';
-    p2score.innerText = '';
+    for (let p of [player1, player2]) {
+        p.score = 0
+        p.display.innerText = '';
+        p.display.classList.remove('loser', 'winner');
+        p.display.classList.add('line-through');
+        p.button.removeAttribute('disabled');
+
+    }
 }
 
